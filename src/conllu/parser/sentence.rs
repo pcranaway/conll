d494aad50a01af008e40560459f871ece3d043ee
alex <pcrunn.alex@gmail.com>
2023-08-26 07:21:30 +0300

    feat: add UPOS parsing

--- src/conllu/parser/sentence.rs
+++ src/conllu/parser/sentence.rs
@@ -1,10 +1,14 @@
+use crate::conllu::treebank::Upos;
 use crate::conllu::treebank::Word;
+use anyhow::{anyhow, Result};
 
 /// Parses a word line, extracting all fields, validating them, and converting them to their
 /// correct form.
-pub fn parse_word(input: &str) -> Option<Word> {
+// TODO: Add context including line number for debugging
+pub fn parse_word(input: &str) -> Result<Word> {
     let split: Vec<&str> = input.split("\t").collect();
 
+    // read all fields
     let id = &split[0];
     let form = &split[1];
     let lemma = &split[2];
@@ -15,6 +19,11 @@ pub fn parse_word(input: &str) -> Option<Word> {
     let deprel = &split[7];
     let deps = &split[8];
     let misc = &split[9];
+
+    // validate fields
+    if !is_valid_id(id) {
+        return Err(anyhow!("invalid word ID"));
+    }
 
     todo!()
 }
@@ -34,6 +43,31 @@ pub fn is_valid_id(input: &str) -> bool {
     }
 
     // TODO: Support decimal numbers (they're used for empty nodes. (must be between 0 and 1))
+}
+
+/// Parses a UPOS field value into its corresponding [Upos] variant.
+pub fn parse_upos(input: &str) -> Result<Upos> {
+    return match input {
+        "ADJ" => Ok(Upos::Adjective),
+        "ADP" => Ok(Upos::Adposition),
+        "ADV" => Ok(Upos::Adverb),
+        "AUX" => Ok(Upos::Auxiliary),
+        "CCONJ" => Ok(Upos::CoordinatingConjunction),
+        "DET" => Ok(Upos::Determiner),
+        "INTJ" => Ok(Upos::Interjection),
+        "NOUN" => Ok(Upos::Noun),
+        "NUM" => Ok(Upos::Numeral),
+        "PART" => Ok(Upos::Particle),
+        "PRON" => Ok(Upos::Pronoun),
+        "PROPN" => Ok(Upos::ProperNoun),
+        "PUNCT" => Ok(Upos::Punctuation),
+        "SCONJ" => Ok(Upos::SubordinatingConjunction),
+        "SYM" => Ok(Upos::Symbol),
+        "VERB" => Ok(Upos::Verb),
+        "X" => Ok(Upos::Other),
+
+        _ => Err(anyhow!("invalid UPOS {}", input)),
+    };
 }
 
 #[cfg(test)]
@@ -56,5 +90,11 @@ mod tests {
         assert!(!is_valid_id("aaa"));
         assert!(!is_valid_id("-34"));
         assert!(!is_valid_id("34-"));
+    }
+
+    #[test]
+    fn test_upos() {
+        assert_eq!(parse_upos("CCONJ").unwrap(), Upos::CoordinatingConjunction);
+        assert!(parse_upos("cconj").is_err());
     }
 }
