4c3783fe72d0889c71bfa8a163750b469c998736
alex <pcrunn.alex@gmail.com>
2023-08-26 08:03:22 +0300

    feat: add parsing method for parsing entire documents

--- src/conllu/parser/mod.rs
+++ src/conllu/parser/mod.rs
@@ -1,11 +1,38 @@
 use self::line::parse_line;
 
+use super::treebank::{Sentence, Treebank};
+
 pub mod line;
+pub mod sentence;
 
 pub fn parse(lines: Vec<String>) {
+    // parsing state
+    let mut treebank = Treebank::default();
+    let mut sentence = Sentence::default();
+
+    // parse lines one by one
     for line in lines {
         let line = parse_line(line);
 
-        dbg!(line);
+        if line.is_none() {
+            continue;
+        }
+
+        match line.unwrap() {
+            line::Line::Comment(_) => {}
+            line::Line::Boundary => {
+                // a boundary line marks the end of a sentence so we finish the sentence, put it
+                // in the treebank, and start a new one.
+
+                treebank.sentences.push(sentence);
+
+                sentence = Sentence::default();
+            }
+            line::Line::Word(line) => {
+                let word = sentence::parse_word(&line).unwrap();
+
+                sentence.words.push(word);
+            }
+        }
     }
 }
