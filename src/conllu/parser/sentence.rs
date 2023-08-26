7a2bcbb91fe016758072703bf5c262d4671ca220
alex <pcrunn.alex@gmail.com>
2023-08-26 08:02:54 +0300

    feat: add parsing and validation for most fields, and also properly parse IDs of empty nodes

--- src/conllu/parser/sentence.rs
+++ src/conllu/parser/sentence.rs
@@ -1,5 +1,4 @@
-use crate::conllu::treebank::Upos;
-use crate::conllu::treebank::Word;
+use crate::conllu::treebank::{Upos, Word};
 use anyhow::{anyhow, Result};
 
 /// Parses a word line, extracting all fields, validating them, and converting them to their
@@ -20,31 +19,100 @@ pub fn parse_word(input: &str) -> Result<Word> {
     let deps = &split[8];
     let misc = &split[9];
 
-    // validate fields
+    // validate id
     if !is_valid_id(id) {
-        return Err(anyhow!("invalid word ID"));
+        return Err(anyhow!("invalid word ID {}", id));
     }
 
-    let upos = parse_upos(upos)?;
+    // create word struct
+    let mut word = Word {
+        id: id.to_string(),
 
-    todo!()
+        ..Default::default()
+    };
+
+    // validate form
+    if !is_unspecified(form) {
+        word.form = Some(form.to_string());
+    }
+
+    // validate lemma
+    if !is_unspecified(lemma) {
+        word.lemma = Some(lemma.to_string());
+    }
+
+    // validate upos
+    if !is_unspecified(upos) {
+        word.upos = Some(parse_upos(upos)?);
+    }
+
+    // validate xpos
+    if !is_unspecified(xpos) {
+        word.xpos = Some(xpos.to_string());
+    }
+
+    // validate feats
+    if !is_unspecified(feats) {
+        // TODO: parse feats
+        word.feats = Some(Vec::default());
+    }
+
+    // validate head
+    if !is_unspecified(head) {
+        if is_valid_id(head) {
+            word.head = Some(head.to_string());
+        } else {
+            return Err(anyhow!("invalid head"));
+        }
+    }
+
+    // validate deprel
+    if !is_unspecified(deprel) {
+        // TODO: parse deprel
+        word.deprel = Some(deprel.to_string());
+    }
+
+    // validate deps
+    if !is_unspecified(deps) {
+        // TODO: parse deps
+        word.deps = Some(deps.to_string());
+    }
+
+    // validate misc
+    if !is_unspecified(misc) {
+        word.misc = Some(misc.to_string());
+    }
+
+    Ok(word)
+}
+
+/// Checks if the given field value is unspecified (denoted by an underscore.)
+pub fn is_unspecified(input: &str) -> bool {
+    return input.trim() == "_";
 }
 
 /// Checks if a string is a valid word ID, as defined by CoNNL-U.
 pub fn is_valid_id(input: &str) -> bool {
-    // range, for multiword tokens
     if input.contains("-") {
+        // range, for multiword tokens
         let mut split = input.split("-");
 
         let lower = split.next().unwrap();
         let upper = split.next().unwrap();
 
         return lower.parse::<u32>().is_ok() && upper.parse::<u32>().is_ok();
+    } else if input.contains(".") {
+        // used for empty nodes?
+        let mut split = input.split(".");
+
+        let lower = split.next().unwrap();
+        let upper = split.next().unwrap();
+
+        return lower.parse::<u32>().is_ok() && upper.parse::<u32>().is_ok();
     } else {
+        // regular id
         return input.parse::<u32>().is_ok();
     }
-
-    // TODO: Support decimal numbers (they're used for empty nodes. (must be between 0 and 1))
 }
 
 /// Parses a UPOS field value into its corresponding [Upos] variant.
@@ -98,5 +166,19 @@ mod tests {
     fn test_upos() {
         assert_eq!(parse_upos("CCONJ").unwrap(), Upos::CoordinatingConjunction);
         assert!(parse_upos("cconj").is_err());
+    }
+
+    #[test]
+    fn test_unspecified() {
+        assert!(!is_unspecified("CCONJ"));
+        assert!(!is_unspecified(""));
+        assert!(!is_unspecified(" "));
+
+        // not sure if we have to trim it
+        assert!(is_unspecified("_ "));
+        assert!(is_unspecified(" _ "));
+        assert!(is_unspecified(" _"));
+
+        assert!(is_unspecified("_"));
     }
 }
