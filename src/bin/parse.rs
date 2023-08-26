cb02d470f8763bfe9e876b24b1b6a934b890d5a5
alex <pcrunn.alex@gmail.com>
2023-08-26 06:28:53 +0300

    feat: add binary for testing

---
+++ src/bin/parse.rs
@@ -0,0 +1,18 @@
+use std::{
+    env,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+use conll::conllu;
+
+fn main() {
+    let file_path = env::args().nth(1).expect("expected file path");
+
+    let file = File::open(file_path).unwrap();
+    let reader = BufReader::new(file);
+
+    let lines: Vec<String> = reader.lines().map(|line| line.unwrap()).collect();
+
+    conllu::parser::parse(lines)
+}
